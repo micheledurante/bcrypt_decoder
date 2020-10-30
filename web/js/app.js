2f353902f6d5377d0236e8e9f98a0797b99416ae
micheled <me@thisisdurante.com>
2020-10-30 23:03:05 +0100

    Remove extra CSS and JS. Add HTML template

--- web/js/app.js
+++ web/js/app.js
@@ -1,8 +1,15 @@
 import init, { greet } from '../wasm/bcrypt_decoder.js';
 
+async function decode(params) {
+    greet(params);
+}
+
 async function run() {
     await init();
-    greet('Rust with WASM');
+
+    document.getElementById('decode').onclick = function() {
+        decode(document.getElementsByName('hash')[0].value);
+    };
 }
 
 run();
